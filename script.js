620ec8a91b004dee7c532e9eb2084a71cf005f0c
MicksTech <accpersonal371@gmail.com>
2025-08-13 14:24:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
 	// Your code here
     //document.getElementById('currentYear').innerText = new Date().getFullYear();
 
+    const openUser = document.getElementById('openUser');
+    const openAdmin = document.getElementById('openAdmin')
 
+    openUser.addEventListener('click', function() {
+        document.getElementById('container-user').style.display = 'flex';
+        document.getElementById('container-login').style.display = 'none';
+    });
+
+    openAdmin.addEventListener('click', function() {
+        document.getElementById('container-login').style.display = 'flex';
+        document.getElementById('container-user').style.display = 'none';
+    });
 });
-
-function login(event) {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    const pass = document.getElementById('pass').value;
-
-    if (email === 'johndeo@gmail.com' && pass === '1234567') {
-        alert('Logged in successfully');
-        window.location.href = 'index.html';
-    } else {
-        alert('Wrong Credentials!');
-        window.location.href = 'Login.html';
-    }
-}
-
-var loginForm = document.getElementById('login-form');
-if (loginForm) {
-    loginForm.addEventListener('submit', login);
-}
